e8158a2ec9b91cf715b749277e0fb7e0fdff962f
SantamariaRamosJ <santamariaramosj@gmail.com>
2021-04-28 17:03:20 -0500

    host fixed

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ const image = require ('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: true,
-      },
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     },
   });
+  
 
   db.select('*').from('users').then(data => {
       console.log(data);
