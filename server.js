43fd75ae0fb1a8b613f2a7ca9695394cb1e3990e
SantamariaRamosJ <santamariaramosj@gmail.com>
2021-04-29 10:44:00 -0500

    server fixed

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const profile = require ('./controllers/profile');
 const image = require ('./controllers/image');
 
 
-// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true,
-      }
-  });
+      ssl: true
+    }
+  })
 
   db.select('*').from('users').then(data => {
       console.log(data);
