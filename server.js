45fffc1f6fc9309ed5f6febb884762d7798194d8
SantamariaRamosJ <santamariaramosj@gmail.com>
2021-04-29 11:47:52 -0500

    fixing server

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ const image = require ('./controllers/image');
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-    },
-      ssl: {
-        rejectUnauthorized: false
-    }
-  })
+client: 'pg',
+connection: {
+  connectionString: process.env.DATABASE_URL,	
+  ssl: true
+}
+});
 
 
   // db.select('*').from('users').then(data => {
@@ -35,10 +33,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => {
-    res.send('this is working');
-});
-
+app.get('/', (req, res) => { res.send('this is working') });
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} );
 
